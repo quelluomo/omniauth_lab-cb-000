575eb637628b468a4ca6edda1cd816fc8f3af55b
Matteo Muscas <thatman.j@gmail.com>
2019-07-10 12:27:59 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,14 @@ class SessionsController < ApplicationController
     render 'welcome/home'
   end
 
+  #solution
+  #def create
+  #  user = User.find_or_create_by(:uid => auth['uid']) do |user|
+  #    user.name = auth['info']['name']
+  #  end
+  #  session[:user_id] = user.try(:id)
+  #end
+
   private
 
   def auth
